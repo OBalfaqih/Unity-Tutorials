3dc6411516d4757b2c32608b1cb5bf490824f521
Omar Balfaqih <obalfaqih@gmail.com>
2017-12-26 02:20:40 +0800

    Update Project Files

---
+++ Unity Tutorials/Intro to UI/Scripts/ColorTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorTest : MonoBehaviour {
+
+	public Material blue;
+	public Material red;
+
+	public void changeBlue(){
+		gameObject.GetComponent<Renderer>().material = blue;
+	}
+
+	public void changeRed(){
+		gameObject.GetComponent<Renderer>().material = red;
+	}
+}
