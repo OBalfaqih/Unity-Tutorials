084b317b29a134e3d5d31955bfcc3be9cce38f55
Omar Balfaqih <obalfaqih@gmail.com>
2018-08-11 02:43:32 +0800

    Adding SavingTutorial script
    This is from this tutorial:
    https://www.youtube.com/watch?v=FwiRf-pvsD4

---
+++ Unity Tutorials/Save&Load/Scripts/SavingTutorial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavingTutorial : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // Check if the key exists
+        if (PlayerPrefs.HasKey("PlayerLevel"))
+        {
+            // If it's there, then load it and store it in a vairable
+            int playerLevel = PlayerPrefs.GetInt("PlayerLevel");
+            print("Player level is " + playerLevel);
+        }else{
+            // Otherwise, we will save the value as a new one
+            PlayerPrefs.SetInt("PlayerLevel", 4);
+            print("Player level was saved successfully");
+        }
+	}
+}
